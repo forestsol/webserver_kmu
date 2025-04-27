6a3cf57bf6b16571c9f50f81add01bf67aff9240
forestsol <51287968+forestsol@users.noreply.github.com>
2025-04-28 02:21:07 +0900

    feat: complete 6,7-week

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotAllowed
 from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.contrib import messages
 
 
 def index(request):
@@ -23,6 +25,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
@@ -30,22 +33,25 @@ def answer_create(request, question_id):
         # 그 안에 데이터 전체가 딕셔너리 형태처럼 저장되어 있음.
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only Post is possible.')
+        form = AnswerForm()
     context = {'question' : question, 'form' : form}
     return render(request, 'pybo/question_detail.html', context)
     # 특히 content라는 변수에 들어있는 정보를 가져오기.
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False) # 임시 저장
+            question.author = request.user
             question.create_date = timezone.now() # 사용자에게 입력받지 않은 현재 시간 데이터를 설정
             question.save() # 데이터 실제 저장
             return redirect('pybo:index')
@@ -53,3 +59,60 @@ def question_create(request):
         form = QuestionForm()  # 빈 폼 생성
     context = {'form' : form}
     return render(request, 'pybo/question_form.html', context)  # 빈 폼을 템플릿에 전달
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method =="POST":
+        form = QuestionForm(request.POST, instance=question)  # instance 기준으로 QuestionForm을 생성 -> 그 뒤에 request.POST의 값으로 덮어쓰라는 뜻.
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)  # get 했을 때, 이전에 적어뒀던 값을 instance 에 담아서 보여줌
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'form' : form}
+    return render(request, 'pybo/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
